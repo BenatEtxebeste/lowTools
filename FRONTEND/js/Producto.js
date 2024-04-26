238bb2149ea053797de45a9085de64dddf3e6edf
Chris2609 <megustas260904@gmail.com>
2024-04-26 13:32:49 +0200

    producos recogidos

--- FRONTEND/js/Producto.js
+++ FRONTEND/js/Producto.js
@@ -1,62 +1,122 @@
+/*
+ CONSULTAS
+*/
 const myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
-let url = (new URL(window.location.origin)).hostname;
+let url = "http://localhost"; //(new URL(window.location.origin)).hostname;
 
-function insertarProcucto(id,nombre, precio, album, categoria) {
-    let formdata = new FormData();
+async function insertarProcucto(id, nombre, precio, album, categoria) {
+  let formdata = new FormData();
+  if (!id == "") {
     formdata.append("id", '"' + id + '"');
-    formdata.append("nombre", '"' + nombre + '"');
-    formdata.append("precio", '"' + precio + '"');
-    formdata.append("album", '"' + album + '"');
-    formdata.append("categoria", '"' + categoria + '"');
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: formdata,
-        redirect: "follow",
-    };
-    fetch(url+":8085/api/insertProducto", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch();
+  }
+  formdata.append("nombre", '"' + nombre + '"');
+  formdata.append("precio", "" + precio + "");
+  formdata.append("album", '"' + album + '"');
+  formdata.append("categoria", '"' + categoria + '"');
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
+  fetch(url + ":8085/api/saveProducto", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((result) => console.log(result));
 }
 
-function listProcuctos() {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-  
-    return fetch(url+":8085/api/index", requestOptions)
-      .then((response) => response.json()) 
-      .then((productos) => { return productos; })
-      .catch();
+async function listProcuctos() {
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  return fetch(url + ":8085/api/index", requestOptions)
+    .then((response) => response.json())
+    .then((productos) => {
+      generarProductos(productos);
+      return JSON.parse(productos);
+    })
+    .catch();
 }
 
-function deleteProducto(id){
-    let formdata = new FormData();
-    formdata.append("id", '"' + id + '"');
+async function deleteProducto(id) {
+  let formdata = new FormData();
+  formdata.append("id", '"' + id + '"');
 
-    const requestOptions = {
-        method: "POST",
-     body: formdata,
-        redirect: "follow"
-    };
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
 
-    fetch(url+":8085/api/deleteProducto", requestOptions)
-        .then((response) => response.json())
-        .then((result) => {return result})
-        .catch();
+  fetch(url + ":8085/api/deleteProducto", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    })
+    .catch();
 }
-function getProcuctos(id) {
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow"
-    };
+async function getProcuctos(id) {
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    fetch(url+":8085/api/getProducto?id="+id, requestOptions)
-        .then((response) => response.json())
-        .then((result) => {return result})
-        .catch();
+  fetch(url + ":8085/api/getProducto?id=" + id, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    })
+    .catch();
 }
+
+/*
+ CARDS
+*/
+
+async function generarProductos(listaProcuctos) {
+  let productos = listaProcuctos["productos"];
+  // Genera las tarjetas HTML para cada producto
+  const productosContainer = document.getElementById("productosContainer");
+  console.log(productos);
+  productos.forEach((producto) => {
+    console.log(producto);
+    let card = document.createElement("div");
+    card.classList.add("card");
+    // Crear imagen
+    var imagen = new Image();
+    imagen.src = producto.album;
+    imagen.classList.add("producto-imagen"); // Agregar clase a la imagen
+
+    // Crear título
+    let titulo = document.createElement("h2");
+    titulo.textContent = producto.nombre;
+    titulo.classList.add("titulo");
+
+    // Crear precio
+    let precio = document.createElement("p");
+    precio.textContent = "Precio: " + producto.precio;
+    precio.classList.add("precio");
+
+    // Crear categoría
+    let categoria = document.createElement("p");
+    categoria.textContent = "Categoría: " + producto.categoria;
+    categoria.classList.add("categoria");
+
+    // Agregar elementos a la tarjeta
+    card.appendChild(imagen);
+    card.appendChild(titulo);
+    card.appendChild(precio);
+    card.appendChild(categoria);
+
+    // Agregar tarjeta al contenedor de productos
+    productosContainer.appendChild(card);
+  });
+}
+
+window.onload = function () {
+  listProcuctos();
+};
