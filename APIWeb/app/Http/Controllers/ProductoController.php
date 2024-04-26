b09f684bfdd511529e160d8dc1670899be6defc7
iker <iker.zamorano18@gmail.com>
2024-04-26 10:39:14 +0200

    Controlador de productos funcional

--- APIWeb/app/Http/Controllers/ProductoController.php
+++ APIWeb/app/Http/Controllers/ProductoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Producto;
+use Illuminate\Support\Facades\Log;
 
 class ProductoController extends Controller
 {
@@ -14,11 +15,43 @@ class ProductoController extends Controller
         return response()->json(['productos' => $productos]);
     }
 
+    public function getProducto(Request $request)
+    {
+        $id = $request->id;
+
+        $producto = Producto::find($id);
+
+        return response()->json($producto);
+    }
+
     public function indexByCategoria(String $categoria, String $name)
     {
         $productos = Producto::where('categoria', $categoria)
                             ->where('name', $name)
                             ->get();
         return response()->json(['productos' => $productos]);
+    }
+
+    public function deleteProducto(Request $request)
+    {
+        $id = $request->id;
+
+        $producto = Producto::where('id', $id)->delete();
+
+        return response()->json(['productos', $producto]);
+    }
+
+    public function insertProducto(Request $request)
+    {
+        $producto = new Producto;
+
+        $producto->nombre = $request->nombre;
+        $producto->precio = $request->precio;
+        $producto->album = $request->album;
+        $producto->categoria = $request->categoria;
+
+        $producto->save();
+
+        return redirect('/getProductos');
     }
 }
