dcfa9b15a318e77eb44b1d3d0136e578ec270196
iker <iker.zamorano18@gmail.com>
2024-05-10 12:00:57 +0200

    Seeder de usuario

--- APIWeb/database/seeders/DatabaseSeeder.php
+++ APIWeb/database/seeders/DatabaseSeeder.php
@@ -12,8 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         $this->call(ProductoSeeder::class);
-
+        $this->call(UserSeeder::class);
     }
 }
